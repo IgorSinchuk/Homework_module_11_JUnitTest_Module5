ae124f8032f2e0b65f863937ad9def9639a2ebab
Igor <OldPal@users.noreply.github.com>
2016-04-15 15:45:42 +0300

    Added files via upload

---
+++ AverageTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+
+public class AverageTest {
+
+    @Test
+    public void testMain() throws Exception {
+
+        float maximum = 10;
+        float minimum = 4;
+        float avg;
+        float sum;
+        float x = 2;
+
+        for(int i=0; i < maximum ; i++) {
+
+        }
+         avg = (minimum*maximum) / x;
+        System.out.println("Среднее " + avg);
+
+    }
+}
